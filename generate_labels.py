dce619f4d997ea218a14c33c86a3455c5d118c95
Chunyu Wang <chunyu.wangdlut@gmail.com>
2019-10-10 17:29:03 +0800

    Update generate_labels.py

--- generate_labels.py
+++ generate_labels.py
@@ -39,7 +39,7 @@ def _weak_project(pose3d, fx, fy, cx, cy):
 
 if __name__ == '__main__':
     subject_list = [1, 5, 6, 7, 8, 9, 11]
-    action_list = [x for x in range(2, 16)]
+    action_list = [x for x in range(2, 17)]
     subaction_list = [x for x in range(1, 3)]
     camera_list = [x for x in range(1, 5)]
 
